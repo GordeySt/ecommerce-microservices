270ae71254363fed2c8e9391d21b51b91e5a5393
gordeyst <gordeystahovets@gmail.com>
2021-07-13 19:54:26 +0300

    Add controller class

---
+++ src/Services/Catalog/Catalog.API/PL/Controllers/CatalogController.cs
@@ -0,0 +1,97 @@
+﻿using Catalog.API.BL.Interface;
+using Catalog.API.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.PL.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(ICatalogService catalogService, ILogger<CatalogController> logger)
+        {
+            _catalogService = catalogService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _catalogService.GetAllProductsAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductById(Guid id)
+        {
+            var product = await _catalogService.GetProductByIdAsync(id);
+
+            if (product is null)
+            {
+                _logger.LogError($"Product with id: {id} not found.");
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [Route("[action]/{categoryName}", Name = "GetProductByCategory")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductByCategory(string categoryName)
+        {
+            var products = await _catalogService.GetProductsByCategory(categoryName);
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _catalogService.AddProductAsync(product);
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            var result = await _catalogService.UpdateProductAsync(product);
+
+            if (result == false)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}", Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+            var result = await _catalogService.DeleteProductAsync(id);
+
+            if (result == false)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
