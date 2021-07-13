5db5d641a8c8ea12ad4817053810a4079982bc25
gordeyst <gordeystahovets@gmail.com>
2021-07-13 19:50:15 +0300

    Add initial implementation of the business layer, add catalog service

---
+++ src/Services/Catalog/Catalog.API/BL/Interface/ICatalogService.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.BL.Interface
+{
+    public interface ICatalogService
+    {
+        Task<IReadOnlyList<Product>> GetAllProductsAsync();
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task<IReadOnlyCollection<Product>> GetProductsByCategory(string categoryName);
+        Task AddProductAsync(Product product);
+        Task<bool> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(Guid id);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/BL/Services/CatalogService.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.API.BL.Interface;
+using Catalog.API.DAL.Entities;
+using Catalog.API.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.BL.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CatalogService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task AddProductAsync(Product product) => 
+            await _productRepository.AddItemAsync(product);
+
+        public async Task<bool> DeleteProductAsync(Guid id) =>
+            await _productRepository.DeleteItemAsync(id);
+
+        public async Task<IReadOnlyList<Product>> GetAllProductsAsync() =>
+            await _productRepository.GetAllItemsAsync();
+
+
+        public async Task<Product> GetProductByIdAsync(Guid id) =>
+            await _productRepository.GetItemByIdAsync(id);
+
+        public async Task<IReadOnlyCollection<Product>> GetProductsByCategory(string categoryName) =>
+            await _productRepository.GetProductsByCategory(categoryName);
+
+        public async Task<bool> UpdateProductAsync(Product product) =>
+            await _productRepository.UpdateItemAsync(product);
+    }
+}
