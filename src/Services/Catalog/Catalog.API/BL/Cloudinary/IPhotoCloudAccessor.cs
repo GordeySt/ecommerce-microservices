d74e20fe1130d5a76e49fda62745671cd24e2e93
gordeyst <gordeystahovets@gmail.com>
2021-07-15 13:20:17 +0300

    Add cloudinary classes to upload images to the cloud and delete them

---
+++ src/Services/Catalog/Catalog.API/BL/Cloudinary/IPhotoCloudAccessor.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.BL.Services;
+using Catalog.API.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Catalog.API.BL.Interfaces
+{
+    public interface IPhotoCloudAccessor
+    {
+        Task<PhotoUploadResult> AddPhotoToCloudAsync(IFormFile file);
+        Task<string> DeletePhotoFromCloudAsync(string publicId);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/BL/Cloudinary/PhotoCloudAccessor.cs
@@ -0,0 +1,64 @@
+﻿using Catalog.API.BL.Interfaces;
+using Catalog.API.Startup.Settings;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Catalog.API.BL.Services
+{
+    public class PhotoCloudAccessor : IPhotoCloudAccessor
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoCloudAccessor(AppSettings appSettings, 
+            IConfiguration configuration)
+        {
+            var account = new Account
+            (
+                appSettings.CloudinarySettings.CloudName,
+                appSettings.CloudinarySettings.ApiKey,
+                configuration["Cloudinary:ApiSecret"]                
+            );
+
+            _cloudinary = new Cloudinary(account);
+        }
+
+        public async Task<PhotoUploadResult> AddPhotoToCloudAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500)
+                            .Crop("fill").Gravity("face")
+                };
+
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+
+            if (uploadResult.Error != null)
+                throw new Exception(uploadResult.Error.Message);
+
+            return new PhotoUploadResult
+            {
+                PublicId = uploadResult.PublicId,
+                Url = uploadResult.SecureUrl.AbsoluteUri
+            };
+        }
+
+        public async Task<string> DeletePhotoFromCloudAsync(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+
+            var result = await _cloudinary.DestroyAsync(deleteParams);
+
+            return result.Result == "ok" ? result.Result : null;
+        }
+    }
+}
