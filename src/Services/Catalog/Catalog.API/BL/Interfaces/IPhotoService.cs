d9c6a4626e8a0b8ba7d173a7c1ccb414ec400aeb
gordeyst <gordeystahovets@gmail.com>
2021-07-15 13:22:27 +0300

    Add service to interact with photos

---
+++ src/Services/Catalog/Catalog.API/BL/Interfaces/IPhotoService.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.API.BL.ResultWrappers;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Catalog.API.BL.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ServiceResult> AddPhotoAsync(IFormFile mainImage, Guid productId);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/BL/Services/PhotoService.cs
@@ -0,0 +1,43 @@
+﻿using Catalog.API.BL.Constants;
+using Catalog.API.BL.Enums;
+using Catalog.API.BL.Interfaces;
+using Catalog.API.BL.ResultWrappers;
+using Catalog.API.DAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Catalog.API.BL.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly IPhotoCloudAccessor _photoCloudAccessor;
+        private readonly IProductRepository _productRepository;
+
+        public PhotoService(IPhotoCloudAccessor photoCloudAccessor, 
+            IProductRepository productRepository)
+        {
+            _photoCloudAccessor = photoCloudAccessor;
+            _productRepository = productRepository;
+        }
+
+        public async Task<ServiceResult> AddPhotoAsync(IFormFile mainImage, Guid productId)
+        {
+            var photoUploadResult = await _photoCloudAccessor.AddPhotoToCloudAsync(mainImage);
+
+            var product = await _productRepository.GetItemByIdAsync(productId);
+
+            if (product is null)
+            {
+                return new ServiceResult(ServiceResultType.NotFound, 
+                    ExceptionMessageConstants.NotFoundItemMessage);
+            }
+
+            product.MainImageUrl = photoUploadResult.Url;
+
+            await _productRepository.UpdateMainImageAsync(product);
+
+            return new ServiceResult(ServiceResultType.Success);
+        }
+    }
+}
