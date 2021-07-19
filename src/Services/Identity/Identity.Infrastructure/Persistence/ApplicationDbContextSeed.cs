e2d004b10c138d26ca12c0ef7206202f6d072f62
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:44:39 +0300

    Add infrastructure layer, implement database context, initial database seeder, configure many-to-many relation between roles and user

---
+++ src/Services/Identity/Identity.Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -0,0 +1,60 @@
+﻿using Identity.Domain.Entities;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Persistence
+{
+    public static class ApplicationDbContextSeed
+    {
+        public static async Task SeedDefaultUsersAsync
+            (UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        {
+            var administratorRole = new ApplicationRole { Name = "Admin" };
+            var userRole = new ApplicationRole { Name = "User" };
+
+            if (roleManager.Roles.All(r => r.Name != administratorRole.Name))
+            {
+                await roleManager.CreateAsync(administratorRole);
+            }
+
+            if (roleManager.Roles.All(r => r.Name != userRole.Name))
+            {
+                await roleManager.CreateAsync(userRole);
+            }
+
+            var administrator = new ApplicationUser
+            {
+                Id = Guid.NewGuid(),
+                UserName = "admin",
+                Email = "administrator@localhost",
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid(),
+                UserName = "user",
+                Email = "user@localhost",
+                EmailConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            if (userManager.Users.All(u => u.UserName != administrator.UserName))
+            {
+                await userManager.CreateAsync(administrator, "pa$$w0rd");
+                await userManager.AddToRolesAsync(administrator, new[] { administratorRole.Name });
+            }
+
+            if (userManager.Users.All(u => u.UserName != user.UserName))
+            {
+                var result = await userManager.CreateAsync(user, "pa$$w0rd");
+                await userManager.AddToRolesAsync(user, new[] { userRole.Name });
+            }
+        }
+    }
+}
---
+++ src/Services/Identity/Identity.Infrastructure/Persistence/Configurations/ApplicationUserRoleConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.Infrastructure.Persistence.Configurations
+{
+    public class AppUserRoleConfiguration : IEntityTypeConfiguration<ApplicationUserRole>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUserRole> builder)
+        {
+             builder
+                .HasKey(pc => new { pc.RoleId, pc.UserId });
+
+            builder
+                .HasOne(x => x.AppRole)
+                .WithMany(x => x.AppUserRoles)
+                .HasForeignKey(x => x.RoleId)
+                .HasPrincipalKey(x => x.Id)
+                .IsRequired();
+
+            builder
+                .HasOne(x => x.AppUser)
+                .WithMany(x => x.AppUserRoles)
+                .HasForeignKey(x => x.UserId)
+                .HasPrincipalKey(x => x.Id)
+                .IsRequired();
+        }
+    }
+}
