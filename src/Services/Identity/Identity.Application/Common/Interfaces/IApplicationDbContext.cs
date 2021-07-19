e16fe212f79605a26bfcd7764f1b388e24430f17
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:42:22 +0300

    Add application layer, add database context interface

---
+++ src/Services/Identity/Identity.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<ApplicationUser> Users { get; set; }
+    }
+}
