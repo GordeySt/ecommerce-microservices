bea06f99712f9ee37eb3b6d9af8469b3049e6d23
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:39:13 +0300

    Configure authentication for the IdentityServer

---
+++ src/Services/Identity/Identity.API/Startup/Configurations/AuthenticationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.API.Startup.Configurations
+{
+    public static class AuthenticationExtensions
+    {
+        public static void RegisterAuthSettings(this IServiceCollection services)
+        {
+            services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = IdentityConstants.ApplicationScheme;
+                o.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            })
+            .AddIdentityCookies();
+        }
+    }
+}
