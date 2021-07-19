191242840f8f75b54c928b2520646b8bca49c8aa
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:40:33 +0300

    Configure IdentityServer, add IdentityUser, Apo Resources, Api Scopes, Clients

---
+++ src/Services/Identity/Identity.API/Startup/Configurations/IdentityServerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Identity.API.Configurations;
+using Identity.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.API.Startup.Configurations
+{
+    public static class IdentityServerExtensions
+    {
+        public static void RegisterIdentityServer(this IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddAspNetIdentity<ApplicationUser>()
+                .AddInMemoryApiResources(IdentityConfiguration.Apis)
+                .AddInMemoryApiScopes(IdentityConfiguration.Scopes)
+                .AddInMemoryClients(IdentityConfiguration.Clients)
+                .AddDeveloperSigningCredential();
+        }
+    }
+}
