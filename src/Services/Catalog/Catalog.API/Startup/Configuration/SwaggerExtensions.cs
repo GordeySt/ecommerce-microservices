3671156be0728583939f10697f85b32284cd3dab
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:45:01 +0300

    Configure Swagger to use OAuth2

--- src/Services/Catalog/Catalog.API/Startup/Configuration/SwaggerExtensions.cs
+++ src/Services/Catalog/Catalog.API/Startup/Configuration/SwaggerExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -18,13 +19,52 @@ namespace Catalog.API.Startup.Configuration
 
                 c.IncludeXmlComments(xmlPath);
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Password = new OpenApiOAuthFlow
+                        {
+                            TokenUrl = new Uri("https://localhost:5010/connect/token"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "catalogapi", "Catalog API" }
+                            }
+                        }
+                    }
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement 
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "oauth2"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
             });
         }
 
         public static void UseSwaggerApplication(this IApplicationBuilder app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1");
+                c.OAuthClientId("client_id_catalog_swagger");
+                c.OAuthClientSecret("client_secret_catalog_swagger");
+            });
         }
     }
 }
