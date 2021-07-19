dfcaae3a247263d7b8413b7218a8e43150c0750e
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:38:39 +0300

    Add application settings and validate them

---
+++ src/Services/Identity/Identity.API/Startup/Settings/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using NetEscapades.Configuration.Validation;
+
+namespace Identity.API.Startup.Settings
+{
+    public class AppSettings : IValidatable
+    {
+        public DbSettings DbSettings { get; set; }
+
+        public void Validate()
+        {
+            DbSettings.Validate();
+        }
+    }
+}
---
+++ src/Services/Identity/Identity.API/Startup/Settings/DbSettings.cs
@@ -0,0 +1,31 @@
+﻿using NetEscapades.Configuration.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Identity.API.Startup.Settings
+{
+    public class DbSettings : IValidatable
+    {
+        [Required]
+        public string Host { get; set; }
+
+        [Required]
+        public int Port { get; set; }
+
+        [Required]
+        public string DatabaseName { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string ConnectionString => $"Server={Host};Port={Port};Database={DatabaseName};" +
+            $"User Id={UserId};Password={Password};";
+
+        public void Validate()
+        {
+            Validator.ValidateObject(this, new ValidationContext(this), true);
+        }
+    }
+}
