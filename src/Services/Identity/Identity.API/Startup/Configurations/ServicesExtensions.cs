2acb41ccf7c5d9c527c6744e8aa76e7e2b29facb
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:40:58 +0300

    Configure application services

---
+++ src/Services/Identity/Identity.API/Startup/Configurations/ServicesExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Identity.API.Services;
+using Identity.API.Startup.Settings;
+using IdentityServer4.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.API.Startup.Configurations
+{
+    public static class ServicesExtensions
+    {
+        public static void RegisterServices(this IServiceCollection services,
+            AppSettings appSettings)
+        {
+            //AppSettings
+            services.AddSingleton(appSettings);
+
+            //Services
+            services.AddScoped<IProfileService, ProfileService>();
+        }
+    }
+}
