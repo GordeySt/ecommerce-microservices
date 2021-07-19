3cd4b426fd2ef7bc53a8ae0ffab59c61f4c3d96c
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:39:55 +0300

    Configure Identity, add users, roles, database context and SignIn manager

---
+++ src/Services/Identity/Identity.API/Startup/Configurations/IdentityExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Identity.Domain.Entities;
+using Identity.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.API.Startup.Configurations
+{
+    public static class IdentityExtensions
+    {
+        public static void RegisterIdentity(this IServiceCollection services)
+        {
+            services
+                .AddIdentityCore<ApplicationUser>(opt =>
+                {
+                    opt.Password.RequireDigit = true;
+                    opt.Password.RequireNonAlphanumeric = false;
+                    opt.Password.RequireUppercase = false;
+                })
+                .AddRoles<ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddSignInManager<SignInManager<ApplicationUser>>();
+        }
+    }
+}
