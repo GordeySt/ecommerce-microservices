1f7d185b10fb91643556b391c4af7a31152417a2
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:41:15 +0300

    Register all services

---
+++ src/Services/Identity/Identity.API/Startup/Startup.cs
@@ -0,0 +1,64 @@
+using Identity.API.Startup.Configurations;
+using Identity.API.Startup.Settings;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Identity.API.Startup
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var appSettings = ReadAppSettings(Configuration);
+
+            services.RegisterDatabase(appSettings);
+            services.RegisterAuthSettings();
+            services.RegisterIdentity();
+            services.RegisterIdentityServer();
+            services.ValidateSettingParameters(Configuration);
+
+            services.RegisterServices(appSettings);
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static AppSettings ReadAppSettings(IConfiguration configuration)
+        {
+            var dbSettings = configuration.GetSection(nameof(AppSettings.DbSettings))
+                .Get<DbSettings>();
+
+            return new AppSettings
+            {
+                DbSettings = dbSettings
+            };
+        }
+    }
+}
