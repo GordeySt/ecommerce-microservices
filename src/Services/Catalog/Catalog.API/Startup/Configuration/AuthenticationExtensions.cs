b4e76c349e2667f75a1b110752b2d19a455de52b
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:45:23 +0300

    Configure authentication, and connection to the identity server as auth server

---
+++ src/Services/Catalog/Catalog.API/Startup/Configuration/AuthenticationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.API.Startup.Configuration
+{
+    public static class AuthenticationExtensions
+    {
+        public static void RegisterAuthSettings(this IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+            })
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "https://localhost:5010";
+                    options.ApiName = "catalogapi";
+                    options.RequireHttpsMetadata = false;
+                    options.RoleClaimType = "role";
+                });
+        }
+    }
+}
