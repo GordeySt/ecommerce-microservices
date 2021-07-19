b1fea1380bc36202702841fcfe14433473c9f77e
gordeyst <gordeystahovets@gmail.com>
2021-07-19 16:37:38 +0300

    Configure IdentityServer Scopes, Apis, Resources, Clients and so on

--- src/Services/Identity/Identity.API/Configurations/IdentityConfiguration.cs
+++ src/Services/Identity/Identity.API/Configurations/IdentityConfiguration.cs
@@ -1,47 +1,28 @@
 ﻿using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Models;
-using IdentityServer4.Test;
 using System.Collections.Generic;
-using System.Security.Claims;
 
 namespace Identity.API.Configurations
 {
     public static class IdentityConfiguration
     {
-		public static List<TestUser> Users => new List<TestUser>
-		
+		public static IEnumerable<IdentityResource> IdentityResources =>
+			new List<IdentityResource>
 			{
-				new TestUser
-                {
-					SubjectId = "fec0a4d6-5830-4eb8-8024-272bd5d6d2bb",
-					Username = "User",
-					Password = "password",
-					Claims = new List<Claim>
-                    {
-						new Claim("role", "User")
-                    }
-                },
-				new TestUser
-                {
-					SubjectId = "c3b7f625-c07f-4d7d-9be1-ddff8ff93b4d",
-					Username = "Admin",
-					Password = "password",
-					Claims = new List<Claim>
-                    {
-						new Claim("role", "Admin")
-                    }
-                }
-		};
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile(),
+				new IdentityResource("roles", new[] { "role" })
+			};
 
 		public static IEnumerable<ApiResource> Apis =>
 			new ApiResource[]
 			{
 				new ApiResource()
                 {
-					Name = "festivalsapi",
+					Name = "catalogapi",
 					Enabled = true,
-					Scopes = new List<string> { "festivalsapi" },
+					Scopes = new List<string> { "catalogapi" },
 					UserClaims = new List<string> { "role" }
                 }
 			};
@@ -49,43 +30,26 @@ namespace Identity.API.Configurations
 		public static IEnumerable<ApiScope> Scopes =>
 			new ApiScope[]
 			{
-				new ApiScope("festivalsapi")
+				new ApiScope("catalogapi")
 			};
 
 		public static IEnumerable<Client> Clients =>
 			new List<Client>
 			{
 				new Client
-				{
-					ClientName = "Postman",
-					AllowOfflineAccess = true,
-					AllowedScopes = new []
+                {
+					ClientId = "client_id_catalog_swagger",
+					ClientSecrets = {  new Secret("client_secret_catalog_swagger".ToSha256()) },
+					AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+					AllowedCorsOrigins = { "http://localhost:5000" },
+					AllowedScopes =
                     {
-						IdentityServerConstants.StandardScopes.OpenId,
-						IdentityServerConstants.StandardScopes.Profile,
 						"roles",
-						"festivalsapi"
-                    },
-					RedirectUris = new []
-                    {
-						"https://www.getpostman.com/oauth2/callback"
-                    },
-					Enabled = true,
-					ClientId = "832afa32-cabe-40a0-8909-2241cd85e47d",
-					ClientSecrets = new []
-                    {
-						new Secret("NotASecret".ToSha256())
-                    },
-					PostLogoutRedirectUris = new []
-                    {
-						"http://localhost:5000/signout-callback-oidc"
-                    },
-					ClientUri = null,
-					AllowedGrantTypes = new []
-                    {
-						GrantType.ResourceOwnerPassword
+						"catalogapi",
+						IdentityServerConstants.StandardScopes.OpenId,
+						IdentityServerConstants.StandardScopes.Profile
                     }
-				}
+                }
 			};
 	}
 }
