c5a9499dda5ad1c2325efbeedca753c969c48198
gordeyst <gordeystahovets@gmail.com>
2021-07-20 21:26:24 +0300

    Add delete role handler

---
+++ src/Services/Identity/Identity.Application/ApplicationRoles/Commands/DeleteRoles/DeleteRoleCommand.cs
@@ -0,0 +1,52 @@
+﻿using Identity.Application.Common;
+using Identity.Application.Common.Utilities;
+using Identity.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Services.Common.Enums;
+using Services.Common.ResultWrappers;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Identity.Application.ApplicationRoles.Commands.DeleteRoles
+{
+    public class DeleteRoleCommand : IRequest<ServiceResult>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class DeleteRoleCommandHandler : IRequestHandler<DeleteRoleCommand, ServiceResult>
+    {
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public DeleteRoleCommandHandler(RoleManager<ApplicationRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<ServiceResult> Handle(DeleteRoleCommand request,
+            CancellationToken cancellationToken)
+        {
+            var role = await _roleManager.Roles
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (role is null)
+            {
+                return new ServiceResult(ServiceResultType.NotFound,
+                    NotFoundExceptionMessageConstants.NotFoundItemMessage);
+            }
+
+            var deleteUserResult = await _roleManager.DeleteAsync(role);
+
+            if (!deleteUserResult.Succeeded)
+            {
+                return new ServiceResult(ServiceResultType.InternalServerError,
+                    DatabaseUtilities.CreateErrorMessage(deleteUserResult.Errors));
+            }
+
+            return new ServiceResult(ServiceResultType.Success);
+        }
+    }
+}
