4cb5340e398e27f65409b47acc315ee82dc2f096
gordeyst <gordeystahovets@gmail.com>
2021-07-20 13:45:02 +0300

    Add implementation of email infrastructure

---
+++ src/Services/Identity/Identity.Infrastructure/Services/Email/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using Identity.Application.Common.Interfaces;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Services.Email
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string userEmail, string emailSubject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("", _config["smtpClientSettings:senderMailLogin"]));
+            emailMessage.To.Add(new MailboxAddress("", userEmail));
+            emailMessage.Subject = emailSubject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using var client = new SmtpClient();
+
+            await client.ConnectAsync(_config["smtpClientSettings:host"], 587, false);
+            client.AuthenticationMechanisms.Remove("XOAUTH2");
+            await client.AuthenticateAsync(_config["smtpClientSettings:senderMailLogin"],
+                _config["smtpClientSettings:senderMailPassword"]);
+            await client.SendAsync(emailMessage);
+
+            await client.DisconnectAsync(true);
+        }
+    }
+}
---
+++ src/Services/Identity/Identity.Infrastructure/Services/Email/EmailService.cs
@@ -0,0 +1,30 @@
+﻿using Identity.Application.Common.Interfaces;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Infrastructure.Services.Email
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IEmailSender _emailSender;
+
+        public EmailService(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public async Task SendEmailVerificationAsync(string token, string origin, string email)
+        {
+            token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+
+            var verifyUrl = $"{origin}/user/verifyEmail?token={token}&email={email}";
+
+            var message = $"<p>Please click the below link to verify your email address:</p><p><a href='{verifyUrl}'>{verifyUrl}></a></p>";
+
+            var subject = "Please verify your email address";
+
+            await _emailSender.SendEmailAsync(email, subject, message);
+        }
+    }
+}
