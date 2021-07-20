4e14df05f7166ec247e4a092d789771028ed5f5a
gordeyst <gordeystahovets@gmail.com>
2021-07-20 21:29:34 +0300

    Add class fot database utilities, add method to create error messages

---
+++ src/Services/Identity/Identity.Application/Common/Utilities/DatabaseUtilities.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Identity.Application.Common.Utilities
+{
+    public static class DatabaseUtilities
+    {
+        public static string CreateErrorMessage(IEnumerable<IdentityError> errors) =>
+            string.Join(",", errors.Select(x => x.Description));
+    }
+}
