b62de39abf23499fa689cff356235cccbd31840f
gordeyst <gordeystahovets@gmail.com>
2021-07-20 15:51:56 +0300

    Add users controller

---
+++ src/Services/Identity/Identity.API/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Identity.Application.ApplicationUsers.Commands.DeleteUsers;
+using Identity.Application.ApplicationUsers.DTOs;
+using Identity.Application.ApplicationUsers.Queries.GetUsersByTokenInfo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Common.Enums;
+using System;
+using System.Threading.Tasks;
+
+namespace Identity.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UsersController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<ApplicationUserDto>> GetUserByTokenInfo()
+        {
+            var result = await Mediator.Send(new GetCurrentUserQuery());
+
+            if (result.Result is ServiceResultType.BadRequest)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return result.Data;
+        }
+
+        [HttpDelete("{id:guid}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteUser(Guid id)
+        {
+            var result = await Mediator.Send(new DeleteUserCommand { Id = id });
+
+            if (result.Result is ServiceResultType.NotFound)
+            {
+                return NotFound(result.Message);
+            }
+
+            if (result.Result is ServiceResultType.BadRequest)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return NoContent();
+        }
+    }
+}
