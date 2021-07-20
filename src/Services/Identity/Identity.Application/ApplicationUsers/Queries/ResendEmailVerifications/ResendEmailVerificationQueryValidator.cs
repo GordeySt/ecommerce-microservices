6df11056186a9a351b7f0b64deac5bcb0e941b92
gordeyst <gordeystahovets@gmail.com>
2021-07-20 13:43:36 +0300

    Add resend email verification handler and validation for data

---
+++ src/Services/Identity/Identity.Application/ApplicationUsers/Queries/ResendEmailVerifications/ResendEmailVerificationQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Identity.Application.Common.Validators;
+
+namespace Identity.Application.ApplicationUsers.Queries.ResendEmailVerifications
+{
+    public class ResendEmailVerificationQueryValidator : AbstractValidator<ResendEmailVerificationQuery>
+    {
+        public ResendEmailVerificationQueryValidator()
+        {
+            RuleFor(v => v.Email).Email();
+        }
+    }
+}
