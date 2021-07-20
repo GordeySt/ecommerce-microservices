c8de03d2a8524cef7bc0521db05deddb989a21a9
gordeyst <gordeystahovets@gmail.com>
2021-07-20 13:44:14 +0300

    Add email infrastructure interfaces

---
+++ src/Services/Identity/Identity.Application/Common/Interfaces/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Identity.Application.Common.Interfaces
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string userEmail, string emailSubject, string message);
+    }
+}
---
+++ src/Services/Identity/Identity.Application/Common/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Identity.Application.Common.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailVerificationAsync(string token, string origin, string email);
+    }
+}
