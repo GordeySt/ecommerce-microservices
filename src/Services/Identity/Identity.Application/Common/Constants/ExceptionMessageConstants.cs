904b331c5650ef49399b7b7a9202ea58ddbe9ca1
gordeyst <gordeystahovets@gmail.com>
2021-07-20 13:43:56 +0300

    Add constants for messages of exceptions

---
+++ src/Services/Identity/Identity.Application/Common/Constants/ExceptionMessageConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity.Application.Common
+{
+    public static class ExceptionMessageConstants
+    {
+        public const string ExistedEmailMessage = "Email already exists";
+        public const string ProblemVerifyingEmailMessage = "Problem verifying email address";
+        public const string ProblemCreatingItemMessage = "Problem creating item";
+    }
+}
