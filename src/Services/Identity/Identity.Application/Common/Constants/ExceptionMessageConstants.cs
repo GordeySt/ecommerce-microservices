e364008d51759d1a95f70733a1b123d490b4a07d
gordeyst <gordeystahovets@gmail.com>
2021-07-20 21:28:37 +0300

    Add separate class for different type of exception messages

--- src/Services/Identity/Identity.Application/Common/Constants/ExceptionMessageConstants.cs
+++ src/Services/Identity/Identity.Application/Common/Constants/ExceptionMessageConstants.cs
@@ -1,12 +1,23 @@
 ﻿namespace Identity.Application.Common
 {
+    public static class NotFoundExceptionMessageConstants
+    {
+        public const string NotFoundItemMessage = "Item not found";
+        public const string NotFoundRoleMessage = "Role not found";
+        public const string NotFoundUserMessage = "User not found";
+    }
+
+    public static class BadRequestExceptionMessageConstants
+    {
+        public const string ProblemVerifyingEmailMessage = "Problem verifying email address";
+        public const string UserIsNotInRoleMessage = "User is not in role";
+        public const string UserIsInRoleMessage = "User is already in this role";
+        public const string RoleAlreadyExistsMessage = "This role already exists";
+    }
+
     public static class ExceptionMessageConstants
     {
         public const string ExistedEmailMessage = "Email already exists";
-        public const string ProblemVerifyingEmailMessage = "Problem verifying email address";
-        public const string ProblemCreatingItemMessage = "Problem creating item";
-        public const string ProblemDeletingItemMessage = "Problem deleting item";
         public const string InvalidTokenMessage = "Invalid token was recieved";
-        public const string NotFoundItemMessage = "Item not found";
     }
 }
