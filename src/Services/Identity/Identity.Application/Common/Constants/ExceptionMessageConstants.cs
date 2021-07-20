6356cbbc43bdad75bb883cdbb8a0659ed2ceb469
gordeyst <gordeystahovets@gmail.com>
2021-07-20 15:50:50 +0300

    Add new exception message constants

--- src/Services/Identity/Identity.Application/Common/Constants/ExceptionMessageConstants.cs
+++ src/Services/Identity/Identity.Application/Common/Constants/ExceptionMessageConstants.cs
@@ -5,5 +5,8 @@
         public const string ExistedEmailMessage = "Email already exists";
         public const string ProblemVerifyingEmailMessage = "Problem verifying email address";
         public const string ProblemCreatingItemMessage = "Problem creating item";
+        public const string ProblemDeletingItemMessage = "Problem deleting item";
+        public const string InvalidTokenMessage = "Invalid token was recieved";
+        public const string NotFoundItemMessage = "Item not found";
     }
 }
