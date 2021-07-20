b0b29114ef7d3b265ba078ac832971940a206a24
gordeyst <gordeystahovets@gmail.com>
2021-07-20 15:50:22 +0300

    Add Dto for the ApplicationUser

---
+++ src/Services/Identity/Identity.Application/ApplicationUsers/DTOs/ApplicationUserDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Identity.Application.Common.Mappings;
+using Identity.Domain.Entities;
+using System;
+
+namespace Identity.Application.ApplicationUsers.DTOs
+{
+    public class ApplicationUserDto : IMapFrom<ApplicationUser>
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<ApplicationUser, ApplicationUserDto>();
+        }
+    }
+}
