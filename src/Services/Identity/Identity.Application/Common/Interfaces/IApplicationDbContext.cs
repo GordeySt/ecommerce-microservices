b3d3b466b64cd2c892355673af3197e32b16e4b0
gordeyst <gordeystahovets@gmail.com>
2021-07-20 15:51:13 +0300

    Add new methods to the database context interface

--- src/Services/Identity/Identity.Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Services/Identity/Identity.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using Identity.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Identity.Application.Common.Interfaces
 {
     public interface IApplicationDbContext
     {
         DbSet<ApplicationUser> Users { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+        EntityEntry<TEntity> Remove<TEntity>(TEntity entity) where TEntity : class;
     }
 }
