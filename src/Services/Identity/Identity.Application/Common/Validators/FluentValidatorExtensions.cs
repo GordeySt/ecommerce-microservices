e79f632a0caf0e20a7afc32fb50a190e6beb39dd
gordeyst <gordeystahovets@gmail.com>
2021-07-20 13:44:36 +0300

    Add extensions for Fluent Validation for password and email

---
+++ src/Services/Identity/Identity.Application/Common/Validators/FluentValidatorExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace Identity.Application.Common.Validators
+{
+    public static class FluentValidatorExtensions
+    {
+        public static IRuleBuilder<T, string> Password<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            var options = ruleBuilder
+               .NotEmpty().WithMessage("Password must not be empty")
+               .Matches("[a-z]").WithMessage("Password must have at least 1 lowercase character")
+               .Matches("[0-9]").WithMessage("Password must contain a number");
+
+            return options;
+        }
+
+        public static IRuleBuilder<T, string> Email<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            var options = ruleBuilder
+                .NotEmpty().WithMessage("Email must not be empty")
+                .EmailAddress().WithMessage("Invalid Email address");
+
+            return options;
+        }
+    }
+}
