d0d7cc63128967f05f799a9a3fc3e61ac889e7c1
gordeyst <gordeystahovets@gmail.com>
2021-07-21 13:35:26 +0300

    Add reset password handler and validators for its data

---
+++ src/Services/Identity/Identity.Application/ApplicationUsers/Commands/ResetPasswords/ResetPasswordCommand.cs
@@ -0,0 +1,52 @@
+﻿using Identity.Application.Common;
+using Identity.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Services.Common.Enums;
+using Services.Common.ResultWrappers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Identity.Application.ApplicationUsers.Commands.ResetPasswords
+{
+    public class ResetPasswordCommand : IRequest<ServiceResult>
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ResetPasswordCommandHandler : IRequestHandler<ResetPasswordCommand, ServiceResult>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ResetPasswordCommandHandler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<ServiceResult> Handle(ResetPasswordCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            if (user is null)
+            {
+                return new ServiceResult(ServiceResultType.NotFound,
+                    NotFoundExceptionMessageConstants.NotFoundUserMessage);
+            }
+
+            var resetPasswordResult = await _userManager
+                .ResetPasswordAsync(user, request.Token, request.Password);
+
+            if (!resetPasswordResult.Succeeded)
+            {
+                return new ServiceResult(ServiceResultType.BadRequest,
+                    BadRequestExceptionMessageConstants.ProblemResetingPasswordMessage);
+            }
+
+            return new ServiceResult(ServiceResultType.Success);
+        }
+    }
+}
---
+++ src/Services/Identity/Identity.Application/ApplicationUsers/Commands/ResetPasswords/ResetPasswordCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Identity.Application.Common.Validators;
+
+namespace Identity.Application.ApplicationUsers.Commands.ResetPasswords
+{
+    public class ResetPasswordCommandValidator : AbstractValidator<ResetPasswordCommand>
+    {
+        public ResetPasswordCommandValidator()
+        {
+            RuleFor(x => x.Email).Email();
+            RuleFor(x => x.Password).Password()
+                .Equal(x => x.ConfirmPassword).WithMessage("Passwords should be equal");
+            RuleFor(x => x.ConfirmPassword).Password();
+            RuleFor(x => x.Token).NotEmpty();
+        }
+    }
+}
