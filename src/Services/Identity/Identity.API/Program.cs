5f829b7084e94569affa8723c42f0ef3d16874f4
gordeyst <gordeystahovets@gmail.com>
2021-07-23 12:46:04 +0300

    Remove seeding commets and seeding class

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -1,14 +1,7 @@
 using Common.Logging;
-using Identity.Domain.Entities;
-using Identity.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using System;
 using System.Threading.Tasks;
 
 namespace Identity.API
@@ -17,34 +10,7 @@ namespace Identity.API
     {
         public async static Task Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            /*using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
-
-                    context.Database.Migrate();
-
-                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                    var roleManager = services.GetRequiredService<RoleManager<ApplicationRole>>();
-
-                    await ApplicationDbContextSeed.SeedDefaultUsersAsync(userManager, roleManager);
-                }
-                catch (Exception ex)
-                {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-
-                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
-
-                    throw;
-                }
-            }*/
-
-            await host.RunAsync();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
