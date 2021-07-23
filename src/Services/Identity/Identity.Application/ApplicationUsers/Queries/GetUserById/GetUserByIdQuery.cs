f733b0cd73ac9898da0985e59fc9dc9f3b257698
gordeyst <gordeystahovets@gmail.com>
2021-07-23 17:41:06 +0300

    Refactor code

---
+++ src/Services/Identity/Identity.Application/ApplicationUsers/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Identity.Domain.Entities;
+using MediatR;
+using Services.Common.ResultWrappers;
+using System;
+
+namespace Identity.Application.ApplicationUsers.Queries.GetUserById
+{
+    public record GetUserByIdQuery(Guid Id) : IRequest<ServiceResult<ApplicationUser>>;
+
+    public class GetUserByIdQueryHandler { }
+}
