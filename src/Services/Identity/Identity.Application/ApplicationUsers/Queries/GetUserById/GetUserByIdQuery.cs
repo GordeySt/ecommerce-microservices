753d040990b64aa24f84cbd1e515029d5932d9d9
gordeyst <gordeystahovets@gmail.com>
2021-07-24 23:28:38 +0300

    Add get user by id handler

--- src/Services/Identity/Identity.Application/ApplicationUsers/Queries/GetUserById/GetUserByIdQuery.cs
+++ src/Services/Identity/Identity.Application/ApplicationUsers/Queries/GetUserById/GetUserByIdQuery.cs
@@ -1,11 +1,41 @@
-﻿using Identity.Domain.Entities;
+﻿using Identity.Application.Common;
+using Identity.Application.Common.Interfaces;
+using Identity.Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Services.Common.Enums;
 using Services.Common.ResultWrappers;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Identity.Application.ApplicationUsers.Queries.GetUserById
 {
     public record GetUserByIdQuery(Guid Id) : IRequest<ServiceResult<ApplicationUser>>;
 
-    public class GetUserByIdQueryHandler { }
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, ServiceResult<ApplicationUser>>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+        public GetUserByIdQueryHandler(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<ServiceResult<ApplicationUser>> Handle(GetUserByIdQuery request, 
+            CancellationToken cancellationToken)
+        {
+            var user = await _dbContext.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (user is null)
+            {
+                return new ServiceResult<ApplicationUser>(ServiceResultType.NotFound,
+                    NotFoundExceptionMessageConstants.NotFoundUserMessage);
+            }
+
+            return new ServiceResult<ApplicationUser>(ServiceResultType.Success, user);
+        }
+    }
 }
