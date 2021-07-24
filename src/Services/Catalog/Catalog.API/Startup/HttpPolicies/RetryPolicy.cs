609b2b61a43f4c9126349fa0e755768d11a99b16
gordeyst <gordeystahovets@gmail.com>
2021-07-24 23:27:47 +0300

    Add http policies for better resilience

---
+++ src/Services/Catalog/Catalog.API/Startup/HttpPolicies/RetryPolicy.cs
@@ -0,0 +1,23 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+using Serilog;
+using System;
+using System.Net.Http;
+
+namespace Catalog.API.Startup.HttpPolicies
+{
+    public static class RetryPolicy
+    {
+        public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy() => 
+            HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(
+                    retryCount: 5,
+                    sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (exception, retryCount, context) =>
+                    {
+                        Log.Error($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}");
+                    }
+                );
+    }
+}
