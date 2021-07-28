4f2c4450a1019de5d15f44f5112b33022238f42c
gordeyst <gordeystahovets@gmail.com>
2021-07-28 13:39:24 +0300

    Add product rating dto and configure mapping for it

---
+++ src/Services/Catalog/Catalog.API/PL/Models/DTOs/ProductRatings/ProductRatingDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Catalog.API.BL.Mappings;
+using Catalog.API.DAL.Entities;
+using Catalog.API.PL.Models.DTOs.Products;
+
+namespace Catalog.API.PL.Models.DTOs.ProductRatings
+{
+    public class ProductRatingDto : IMapFrom<ProductRating>
+    {
+        public int Rating { get; set; }
+        public ProductDto Product { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<ProductRating, ProductRatingDto>()
+                .ForMember(d => d.Product, options => options.MapFrom(source => source.Product));
+        }
+    }
+}
