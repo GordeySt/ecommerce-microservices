e280898faad419f60b5636274f0112d16c42a6e1
gordeyst <gordeystahovets@gmail.com>
2021-08-10 18:58:07 +0300

    Fix eslint configuration and webpack configuration

--- src/WebApps/react-app/webpack/webpack.common.js
+++ src/WebApps/react-app/webpack/webpack.common.js
@@ -3,41 +3,46 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts)x?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
-      },
+    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ['ts-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'src', to: 'build' }],
+        }),
     ],
-  },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
-    }),
-    new CopyPlugin({
-      patterns: [{ from: 'src', to: 'build' }],
-    }),
-  ],
 }
--- src/WebApps/react-app/webpack/webpack.config.js
+++ src/WebApps/react-app/webpack/webpack.config.js
@@ -2,9 +2,9 @@
 const commonConfig = require('./webpack.common.js')
 
 module.exports = (envVars) => {
-  const { env } = envVars
-  const envConfig = require(`./webpack.${env}.js`)
-  const config = merge(commonConfig, envConfig)
+    const { env } = envVars
+    const envConfig = require(`./webpack.${env}.js`)
+    const config = merge(commonConfig, envConfig)
 
-  return config
+    return config
 }
