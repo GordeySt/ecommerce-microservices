c2a9ae0520db09e23beb7ae7ccff14f7c4d30c49
gordeyst <gordeystahovets@gmail.com>
2021-08-15 15:09:09 +0300

    Add base api configuration and handling errors from api

---
+++ src/WebApps/react-app/src/common/api/baseApi.ts
@@ -0,0 +1,32 @@
+﻿import axios, { AxiosResponse } from 'axios'
+import { ErrorsHandler } from './errors'
+
+axios.defaults.baseURL = 'http://localhost:5015/'
+
+axios.interceptors.response.use(
+    async (response) => {
+        return response
+    },
+    (er) => {
+        ErrorsHandler.handleNetworkError(er)
+        ErrorsHandler.handle404Error(er.response)
+        ErrorsHandler.handle400Error(er.response)
+        ErrorsHandler.handle500Error(er.response)
+        ErrorsHandler.handle401Error(er.response)
+
+        throw er.response
+    }
+)
+
+const sleep = (ms: number) => (response: AxiosResponse) =>
+    new Promise<AxiosResponse>((resolve) => setTimeout(() => resolve(response), ms))
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data
+
+export const requests = {
+    get: <T>(url: string) => axios.get<T>(url).then(sleep(1000)).then(responseBody),
+    post: <T>(url: string, body: Record<string, never>) =>
+        axios.post<T>(url, body).then(sleep(1000)).then(responseBody),
+    put: <T>(url: string, body: Record<string, never>) => axios.put<T>(url, body).then(sleep(1000)).then(responseBody),
+    del: <T>(url: string) => axios.delete<T>(url).then(sleep(1000)).then(responseBody),
+}
---
+++ src/WebApps/react-app/src/common/api/errors.ts
@@ -0,0 +1,26 @@
+﻿import { AxiosError, AxiosResponse } from 'axios'
+import { toast } from 'react-toastify'
+import { history } from '../..'
+
+export const ErrorsHandler = {
+    handleNetworkError: (error: AxiosError) =>
+        error.message === 'Network Error' && !error.response && toast.error('Network error - make sure API is running'),
+    handle404Error: (response: AxiosResponse) => response.status === 404 && history.push('/notfound'),
+    handle400Error: (response: AxiosResponse) =>
+        response.status === 400 &&
+        response.config.method === 'get' &&
+        response.data.errors.hasOwnProperty('id') &&
+        history.push('/notfound'),
+    handle500Error: (response: AxiosResponse) =>
+        response.status === 500 && toast.error('Server error - check the terminal for more info!'),
+    handle401Error: (response: AxiosResponse) => {
+        if (
+            response.status === 401 &&
+            response.headers['www-authenticate'].startsWith('Bearer error="invalid_token"')
+        ) {
+            window.localStorage.removeItem('jwt')
+            history.push('/')
+            toast.info('Your session has expired, please login again')
+        }
+    },
+}
