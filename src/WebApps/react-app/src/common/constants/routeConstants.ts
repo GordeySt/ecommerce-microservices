29ae5596a6a412db308dcf579a1ac7dfcccf0770
gordeyst <gordeystahovets@gmail.com>
2021-08-27 14:13:42 +0300

    Add routes for oidc client components and welcome-page

--- src/WebApps/react-app/src/common/constants/routeConstants.ts
+++ src/WebApps/react-app/src/common/constants/routeConstants.ts
@@ -1,17 +1,18 @@
 ﻿export const CommonRoutes = {
     defaultRoute: '/',
     notFoundRoute: '/notfound',
+    welcomePageRoute: '/welcome-page',
 };
 
 export const AuthRoutes = {
-    signInRoute: '/auth/sign-in',
     verifyEmailRoute: '/auth/verify-email',
     signUpSuccessRoute: '/auth/signup-success',
+    signInOidcRoute: '/signin-oidc',
+    signOutOidcRoute: '/signout-oidc',
 };
 
 export const AuthApiUrls = {
     signUpUrl: '/auth/signup',
-    signInUrl: '/auth/signin',
     resendEmailVerificationUrl: '/auth/resend-email-verification?email=',
     verifyEmailUrl: '/auth/verify-email',
 };
