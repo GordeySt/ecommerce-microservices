3ec7d6b701b1500af215f11d5e5ca82f17a3dce8
gordeyst <gordeystahovets@gmail.com>
2021-09-07 10:06:53 +0300

    Add product model

---
+++ src/WebApps/react-app/src/common/models/product.ts
@@ -0,0 +1,12 @@
+﻿export interface IProduct {
+    id: string;
+    name: string;
+    category: string;
+    summary: string;
+    description: string;
+    mainImageUrl: string;
+    price: number;
+    ageRating: number;
+    count: number;
+    averageRating: number;
+}
