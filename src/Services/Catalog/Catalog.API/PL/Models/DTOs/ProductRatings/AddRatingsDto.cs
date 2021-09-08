937c8fbc031e8e137c6619cdc5800fc287310d73
gordeyst <gordeystahovets@gmail.com>
2021-09-08 11:21:20 +0300

    Change product ratings endpoints and update xml comments

---
+++ src/Services/Catalog/Catalog.API/PL/Models/DTOs/ProductRatings/AddRatingsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Catalog.API.PL.Models.DTOs.ProductRatings
+{
+
+    public class AddRatingsDto
+    {
+        public Guid Id { get; set; }
+        public int RatingCount { get; set; }
+    }
+
+}
