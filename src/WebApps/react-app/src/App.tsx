2907aecbdc30aa7b2c6c600cbde6f46b05f2611c
gordeyst <gordeystahovets@gmail.com>
2021-09-08 11:23:29 +0300

    Add new routes

--- src/WebApps/react-app/src/App.tsx
+++ src/WebApps/react-app/src/App.tsx
@@ -4,16 +4,17 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import SignUpSuccess from './components/auth/SignUpSuccess';
 import VerifyEmail from './components/auth/VerifyEmail';
-import { AuthRoutes, CommonRoutes } from './common/constants/routeConstants';
+import { AuthRoutes, CatalogRoutes, CommonRoutes } from './common/constants/routeConstants';
 import NotFound from './common/layout/NotFound';
 import StartPage from './pages/start-page/StartPage';
 import userManager, { loadUser } from './common/auth/userService';
 import SignInOidc from './common/auth/SignInOidc';
 import AuthProvider from './common/auth/authProvider';
 import SignoutOidc from './common/auth/SignoutOidc';
-import NavBar from './common/layout/NavBar';
 import PrivateRoute from './common/layout/PrivateRoute';
+import { WelcomePage } from './pages/welcome-page/WelcomePage';
 import CatalogPage from './pages/catalog-page/CatalogPage';
+import NavBar from './common/layout/NavBar';
 
 const App = () => {
     useEffect(() => {
@@ -33,7 +34,8 @@ const App = () => {
                                 <NavBar />
                                 <Container maxWidth="lg">
                                     <Switch>
-                                        <PrivateRoute path={CommonRoutes.welcomePageRoute} component={CatalogPage} />
+                                        <PrivateRoute path={CommonRoutes.welcomePageRoute} component={WelcomePage} />
+                                        <Route path={CatalogRoutes.catalogPageRoute} component={CatalogPage} />
                                         <Route path={AuthRoutes.signInOidcRoute} component={SignInOidc} />
                                         <Route path={AuthRoutes.signOutOidcRoute} component={SignoutOidc} />
                                         <Route path={AuthRoutes.signUpSuccessRoute} component={SignUpSuccess} />
