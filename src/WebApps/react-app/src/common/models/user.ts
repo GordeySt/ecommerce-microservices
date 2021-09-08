0d420e8630bc4cf28c668e1ec7fcf0f9c0bfd7f1
gordeyst <gordeystahovets@gmail.com>
2021-09-08 11:27:12 +0300

    Add new models for rating and user

--- src/WebApps/react-app/src/common/models/user.ts
+++ src/WebApps/react-app/src/common/models/user.ts
@@ -1,4 +1,12 @@
-﻿export interface IUserFormValues {
+﻿import { IProductRating } from './rating';
+
+export interface IUserFormValues {
     email: string;
     password: string;
+}
+
+export interface ICurrentUser {
+    id: string;
+    userName: string;
+    ratings: IProductRating[];
 }
