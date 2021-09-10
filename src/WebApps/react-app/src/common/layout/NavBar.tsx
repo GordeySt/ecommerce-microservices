3364555889861f4a885ccf7fe5ab8802eb501431
gordeyst <gordeystahovets@gmail.com>
2021-09-10 12:34:46 +0300

    Change style to the class name

--- src/WebApps/react-app/src/common/layout/NavBar.tsx
+++ src/WebApps/react-app/src/common/layout/NavBar.tsx
@@ -18,6 +18,9 @@ const useStyles = makeStyles((theme: Theme) =>
         title: {
             flexGrow: 1,
         },
+        appBar: {
+            marginBottom: '20px',
+        },
     })
 );
 
@@ -36,7 +39,7 @@ const NavBar = () => {
     };
     return (
         <div className={classes.root}>
-            <AppBar position="static" style={{ marginBottom: '20px' }}>
+            <AppBar position="static" className={classes.appBar}>
                 <Container maxWidth="lg">
                     <Toolbar>
                         <Typography variant="h6" className={classes.title}>
