ece8e5f072fef4a61fc3ae94e30917ea3226ed8b
gordeyst <gordeystahovets@gmail.com>
2021-09-10 12:19:22 +0300

    Change empty object to the unknown type

--- src/WebApps/react-app/src/common/auth/SignInOidc.tsx
+++ src/WebApps/react-app/src/common/auth/SignInOidc.tsx
@@ -4,7 +4,7 @@ import { CommonRoutes } from '../constants/routeConstants';
 import Loader from '../layout/Loader';
 import { signinRedirectCallback } from './userService';
 
-const SigninOidc: FC<{}> = () => {
+const SigninOidc: FC<unknown> = () => {
     const history = useHistory();
     useEffect(() => {
         async function signinAsync() {
