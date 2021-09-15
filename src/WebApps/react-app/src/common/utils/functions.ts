ee68010297db56d365045415d3cfbc2720c6e3d6
gordeyst <gordeystahovets@gmail.com>
2021-09-15 16:46:16 +0300

    Add utils function for forming url search params

---
+++ src/WebApps/react-app/src/common/utils/functions.ts
@@ -0,0 +1,14 @@
+﻿import { IPredicate } from '../../components/catalog/state/reducers/productsReducer';
+import { PagingParams } from '../models/pagination';
+
+export const formUrlSearchParams = (pagingParams: PagingParams, predicates: IPredicate[]) => {
+    const params = new URLSearchParams();
+
+    params.append('pageSize', pagingParams.pageSize.toString());
+    params.append('pageNumber', pagingParams.pageNumber.toString());
+    predicates.map((predicate) => {
+        return params.append(predicate.key, predicate.value);
+    });
+
+    return params;
+};
